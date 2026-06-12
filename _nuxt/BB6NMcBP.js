6a5a49aba766747d2460ad0b77b27a7d1b2b4796
Alexandre Matencio <amatencio@pm.me>
2026-06-12 10:51:43 +0200

    deploy

---
+++ _nuxt/BB6NMcBP.js
@@ -0,0 +1,1 @@
+import{_ as s,o,c as r,J as t}from"./Cwv-PQhJ.js";const n={};function c(e,a){return o(),r("th",null,[t(e.$slots,"default")])}const l=Object.assign(s(n,[["render",c]]),{__name:"ProseTh"});export{l as default};
---
+++ _nuxt/BX6pnxdp.js
@@ -0,0 +1,1 @@
+import{_ as r,o as s,c as o,J as t}from"./Cwv-PQhJ.js";const n={};function c(e,a){return s(),o("tr",null,[t(e.$slots,"default")])}const l=Object.assign(r(n,[["render",c]]),{__name:"ProseTr"});export{l as default};
